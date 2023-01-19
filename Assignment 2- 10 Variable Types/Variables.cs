2241e3589c0781c8dd7d1906493c385b90375962
DigitalCat404 <10807177@uvu.edu>
2023-01-19 14:34:21 -0700

    Created Assignment 2
    Variables.cs

---
+++ Assignment 2- 10 Variable Types/Variables.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace Variables{
+  class Program{
+    static void Main(string[] args){
+    //declare variables
+    int one, two;
+    float floater;
+    double doubler;
+    char character, letter;
+    string mean, bean;
+    bool truth, lie;
+
+    //assign variables
+    one = 1;
+    two = 2;
+    floater = 1.5;
+    doubler = 5.9;
+    character = 'c';
+    letter = 'L';
+    mean = "This is rude.";
+    bean = "It's in coffee.";
+    truth = true;
+    lie = false;
+
+      Console.WriteLine(one);
+      Console.WriteLine(two);
+      Console.WriteLine(floater);
+      Console.WriteLine(doubler);
+      Console.WriteLine(character);
+      Console.WriteLine(letter);
+      Console.WriteLine(mean);
+      Console.WriteLine(bean);
+      Console.WriteLine(truth);
+      Console.WriteLine(lie);
+    }
+  }
+}
