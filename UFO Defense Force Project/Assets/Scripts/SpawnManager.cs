ea202e8c1f59bac5343f67eb6ac31e6048a35aa2
DigitalCat404 <10807177@uvu.edu>
2023-02-17 18:52:29 -0700

    Challenge 6- Pickup Spawner
    Renamed EnemySpawnManager to just SpawnManager to handle pickup

--- UFO Defense Force Project/Assets/Scripts/SpawnManager.cs
+++ UFO Defense Force Project/Assets/Scripts/SpawnManager.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawnManager : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
     public GameObject[] enemyPrefabs;
+    //in case more pickups are added, futureproofed with array
+    public GameObject[] pickupPrefabs;
 
     //make private variable visible and editable in builder, Serialize
     [SerializeField]
@@ -15,11 +17,15 @@ public class EnemySpawnManager : MonoBehaviour
     private float startDelay = 2f;
     private float spawnInterval = 2f;
 
+    private float pickupDelay = 5f;
+    private float pickupInterval = 15f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnRandomEnemy", startDelay, spawnInterval);
+        InvokeRepeating("SpawnPickup", pickupDelay, pickupInterval);
     }
 
     void SpawnRandomEnemy(){
@@ -31,5 +37,13 @@ public class EnemySpawnManager : MonoBehaviour
     
         //Spawn the enemy selected
         Instantiate(enemyPrefabs[enemyIndex], spawnPos, enemyPrefabs[enemyIndex].transform.rotation);
+    }
+
+    void SpawnPickup(){
+        //Generate a position to spawn at, with random X
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+    
+        //Spawn the pickup
+        Instantiate(pickupPrefabs[0], spawnPos, pickupPrefabs[0].transform.rotation);
     }
 }
