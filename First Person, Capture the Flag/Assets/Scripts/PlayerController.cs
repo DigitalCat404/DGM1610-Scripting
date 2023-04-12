1c33facf5f3519f0565375884d015e66c7169cf1
DigitalCat404 <10807177@uvu.edu>
2023-04-12 16:32:14 -0600

    4/11/23 Class- Started FPS Capture the Flag, Updated UFO Game
    UFO changes:
    1) UFO collisions on player give 1/10 points of shooting them
    2) Addressed bugs related to Pickup item not interacting correctly with player and awarding points

---
+++ First Person, Capture the Flag/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Stats")]
+    public float moveSpeed; //units per second
+    public float jumpForce;
+
+    public int maxHP;
+    public int curHP;
+
+    [Header("Mouse Look")]
+    public float lookSensitivity;
+    public float maxLookX;  //lowest down we can look
+    public float minLookX;  //highest up we can look
+    private float rotX;     //current X rotation of camera
+
+    //References
+    private Camera camera;
+    private Rigidbody rb;
+    //private Weapon weapon;
+
+
+    void Awake(){
+        //weapon = GetComponent<Weapon>();
+        curHP = maxHP;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+        rb = GetComponent<Rigidbody>();
+
+        /* Initialize UI
+        GameUI.instnace.UpdateHealthBar(curHP, maxHP);
+        GameUI.instance.UpdateScoreText(0);
+        GameUI.instance.UpdateAmmoText(weapon.curAmmo, weapon.maxAmmo); */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CamLook();
+
+        if(Input.GetKeyDown("Space")){
+            Jump();
+        }
+
+        //Fire weapon
+    }
+
+
+    void Move(){
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float z = Input.GetAxis("Vertical") * moveSpeed;
+
+        //move by manipulating rigidbody relative to camera
+        Vector3 dir = transform.right * x + transform.forward * z;
+
+        dir.y = rb.velocity.y;
+        rb.velocity = dir;  //apply force in direction of camera
+    }
+
+
+    void CamLook(){
+        float y = Input.GetAxis("Mouse X") * lookSensitivity;
+        rotX += Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        //clamp restricts movement of camera
+        rotX = Mathf.Clamp(rotX, minLookX, maxLookX);
+
+        //use .eulerAngles over .Rotate
+        camera.transform.localRotation = Quaternion.Euler(-rotX,0,0);
+        transform.eulerAngles += Vector3.up * y;
+    }
+
+
+    void Jump(){
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        //if on ground, allow jump
+        if(Physics.Raycast(ray, 1.1f)){
+            //add force to jump
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    
+    public void TakeDamage(int damage){
+        curHP -= damage;
+
+        if(curHP <= 0){
+            //Die();
+        }
+    }
+}
--- UFO Defense Force Project/Assets/Scripts/DetectCollisions.cs
+++ UFO Defense Force Project/Assets/Scripts/DetectCollisions.cs
@@ -19,18 +19,23 @@ public class DetectCollisions : MonoBehaviour
     }
 
     private void OnTriggerEnter(Collider other){
+        
+        if(tag != "Pickup"){
+            //if hit by an attack, and not a Pickup, decrement health and delete projectile
+            if((other.gameObject.CompareTag("Projectile"))&&(tag != "Pickup")){
+                healthValue--;
 
-        //if hit by an attack, and not a Pickup,  decrement health and delete projectile
-        if((other.gameObject.CompareTag("Projectile"))&&(tag != "Pickup")){
-            healthValue--;
+                //play hit audio
 
-            //play hit audio
+                //delete impacting projectile
+                Destroy(other.gameObject);
 
-            //delete impacting projectile
-            Destroy(other.gameObject);
-
-        //if hitting a player, self-destruct
-        } else if(other.gameObject.CompareTag("Player")){
+            //if hitting a player, self-destruct AND SHRINK SCORE VALUE
+            } else if(other.gameObject.CompareTag("Player")){
+                healthValue = 0;
+                scoreToGive /= 10;
+            }
+        } else if(other.gameObject.CompareTag("Player")) {
             healthValue = 0;
         }
 
@@ -44,7 +49,11 @@ public class DetectCollisions : MonoBehaviour
     //if out of health, delete UFO
     void CheckDeath(){
         if(healthValue <= 0){
-            Explosion();
+
+            if(tag != "Pickup"){
+                Explosion();
+            }
+
             scoreManager.IncreaseScore(scoreToGive);
             Destroy(this.gameObject);
         }
